ebd246213e6a56a128c2adbd329f657a967d52f8
thanhit95 <thanh.it1995@gmail.com>
2021-04-11 10:35:26 +0700

    non-recursive traveler

---
+++ bintree_java/src/binarytree/traveler/NonRecurTraveler.java
@@ -0,0 +1,81 @@
+package binarytree.traveler;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import binarytree.BinNode;
+import java.util.Stack;
+
+
+
+public class NonRecurTraveler< TKey extends Number & Comparable<? super TKey>,
+                            TNode extends BinNode<TKey, TNode>
+                          >
+             extends Traveler<TKey, TNode>
+{
+
+
+    protected List<TKey> resPath;
+
+
+
+    @Override
+    public List<TKey> traverse(TNode root, OrderTraverse order) {
+        resPath = new ArrayList<>();
+
+        switch (order) {
+        case PRE:
+
+            break;
+
+        case IN:
+            traverseIn(root);
+            break;
+
+        case POST:
+
+            break;
+
+        default:
+            break;
+        }
+
+        return resPath;
+    }
+
+
+
+    protected void traversePre(TNode node) {
+        // not implemented yet
+    }
+
+
+
+    protected void traverseIn(TNode node) {
+        var stack = new Stack<TNode>();
+
+        while (true) {
+            while (null != node) {
+                stack.add(node);
+                node = node.left;
+            }
+
+            if (stack.empty()) {
+                break;
+            }
+
+            TNode resultNode = stack.pop();
+            resPath.add(resultNode.key);
+
+            node = resultNode.right;
+        }
+
+        return;
+    }
+
+
+
+    protected void traversePost(TNode node) {
+        // not implemented yet
+    }
+}
